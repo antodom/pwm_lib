2ea3b110df5d737bf18aee1677e216b3b07f25df
Antonio Carlos Domínguez Brito <adominguez@iusiani.ulpgc.es>
2016-06-12 13:17:33 +0100

    Changes:
    *  Solved   a	weird	error	when   compiling   example   basic_test.ino   in
      Arduino   IDE.    It	 is   related	to   the   preprocessed    version    of
      basic_test.ino  where  introduce  a  prototype   for	 a   template	function

--- examples/basic_test/basic_test.ino
+++ examples/basic_test/basic_test.ino
@@ -89,8 +89,11 @@ void setup() {
   pwm_pin42.start(PWM_PERIOD_PIN_42,PWM_DUTY_PIN_42);
 }
 
-template<typename pwm_type>
-void change_duty(pwm_type& pwm_obj,uint32_t pwm_duty,uint32_t pwm_period)
+template<typename pwm_type> void change_duty(
+  pwm_type& pwm_obj,
+  uint32_t pwm_duty,
+  uint32_t pwm_period
+)
 {
   uint32_t duty=pwm_obj.get_duty()+pwm_duty;
   if(duty>pwm_period) duty=pwm_duty;
